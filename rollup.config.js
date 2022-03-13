6d14f57bcd47524f856b5bcc10b26622416f8302
raphael.jorge <27439414+raphael-jorge@users.noreply.github.com>
2022-03-12 23:32:40 -0300

    build: setup rollup

---
+++ rollup.config.js
@@ -0,0 +1,72 @@
+import typescript from 'rollup-plugin-typescript2';
+import { terser } from 'rollup-plugin-terser';
+import dts from 'rollup-plugin-dts';
+
+const terserPlugin = terser({
+  format: {
+    comments: false,
+  },
+});
+
+const globals = {
+  tablab: 'Tablab',
+};
+
+export default [
+  {
+    input: 'src/index.ts',
+    external: ['tablab'],
+    output: [
+      {
+        file: 'lib/index.cjs.js',
+        format: 'cjs',
+      },
+      {
+        file: 'lib/index.cjs.min.js',
+        format: 'cjs',
+        plugins: [terserPlugin],
+      },
+      {
+        file: 'lib/index.es.js',
+        format: 'es',
+      },
+      {
+        file: 'lib/index.es.min.js',
+        format: 'es',
+        plugins: [terserPlugin],
+      },
+      {
+        file: 'lib/index.umd.js',
+        format: 'umd',
+        name: 'TablabI18n',
+        globals,
+      },
+      {
+        file: 'lib/index.umd.min.js',
+        format: 'umd',
+        name: 'TablabI18n',
+        globals,
+        plugins: [terserPlugin],
+      },
+      {
+        file: 'lib/index.global.js',
+        format: 'iife',
+        name: 'TablabI18n',
+        globals,
+      },
+      {
+        file: 'lib/index.global.min.js',
+        format: 'iife',
+        name: 'TablabI18n',
+        globals,
+        plugins: [terserPlugin],
+      },
+    ],
+    plugins: [typescript({ tsconfig: './tsconfig.build.json' })],
+  },
+  {
+    input: 'lib/index.d.ts',
+    output: [{ file: 'lib/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
